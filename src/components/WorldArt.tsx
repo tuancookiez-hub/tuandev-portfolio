6e1d9a851633ba3fa6e22975cb3da8a10ace8275
Tuna Dev <tuancookiez@gmail.com>
2026-08-18 03:43:22 +0800

    Restore spatial page-top crop: left card = left of page

--- src/components/WorldArt.tsx
+++ src/components/WorldArt.tsx
@@ -2,27 +2,13 @@ import type { WorldId } from "../data/worlds";
 
 export default function WorldArt({ id }: { id: WorldId }) {
   const face = id !== "robotics";
-  if (!face) {
-    return (
-      <img
-        className="gateway-image"
-        data-face={false}
-        src={`./assets/gateway/${id}.webp`}
-        alt=""
-        aria-hidden="true"
-      />
-    );
-  }
   return (
-    <picture>
-      <source media="(max-width: 720px)" srcSet={`./assets/gateway/${id}-face-m.webp`} />
-      <img
-        className="gateway-image"
-        data-face={true}
-        src={`./assets/gateway/${id}-face.webp`}
-        alt=""
-        aria-hidden="true"
-      />
-    </picture>
+    <img
+      className="gateway-image"
+      data-face={face}
+      src={face ? `./assets/gateway/${id}-face.webp` : `./assets/gateway/${id}.webp`}
+      alt=""
+      aria-hidden="true"
+    />
   );
 }
